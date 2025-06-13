bd40a17ba532c44b7b135635009231c63be8c555
davidechiego <d.chiego.lmu@gmail.com>
2025-06-13 11:25:51 +0200

    Tests

--- src/solitons1d/soliton.py
+++ src/solitons1d/soliton.py
@@ -42,3 +42,38 @@ class Soliton():
          self.energy = total_energy
     # Now we have a method that takes in the profile function, sums it up, multiplies this value by the grid spacing, stores the value in self.energy then returns this value.
 
+
+# A user can create a grid by specifying the number of grid points and the grid spacing. The Grid will then compute a grid (saved as numpy array) and save the length of the grid.
+
+class Grid:
+    """
+    A 1D grid.
+
+    Parameters
+    ----------
+    num_grid_points : int
+        Number of grid points used in grid.
+    grid_spacing : float
+        Spacing between grid points.
+
+    Attributes
+    ----------
+    grid_length : float
+        Total length of grid.
+    grid_points : np.array[float]
+        An array of grid points, of the grid.
+
+    """
+
+    def __init__(
+        self,
+        num_grid_points: int,
+        grid_spacing: float,
+    ):
+        self.num_grid_points = num_grid_points
+        self.grid_spacing = grid_spacing
+        self.grid_length = (num_grid_points) * grid_spacing
+
+        self.grid_points = np.arange(
+            -self.grid_length / 2, self.grid_length / 2, grid_spacing
+        )
